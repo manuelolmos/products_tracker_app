be6d73ec78d585a88c1e96a782af1ec9d9cd53bb
Manuel Olmos Gil <manuolmos88@gmail.com>
2018-11-10 01:43:15 -0300

    Add initial project structure

---
+++ ProductsTracker/Classes/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProductsTracker
+//
+//  Created by Manuel Olmos Gil on 10/11/2018.
+//  Copyright © 2018 Manuel Olmos Gil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
